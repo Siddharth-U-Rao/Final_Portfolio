ff47a8b432cd08317bff36caaaa486643012a322
Siddharth U Rao K <raosiddharth2004@gmail.com>
2025-08-05 18:46:25 +0530

    Replaced resume PDF

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,7 +15,7 @@ export const AboutSection = () => {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">Get In Touch</a>
-              <a  href="./Resume/Siddharth_U_Rao_K's Resume.pdf"
+              <a  href="./Resume/Siddharth_Resume.pdf"
   download target="_blank" rel="noopener noreferrer"className="px-6 py-2 rounded-full border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download Resume</a>
             </div>
           </div>
